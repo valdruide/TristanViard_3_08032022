b1a7a771de1cc03f4ba18c9f96d2b3d1a3defd11
Valdruide <tviardprimo190@gmail.com>
2022-03-07 16:16:27 +0100

    palette de gout

--- la-palette-du-gout.php
+++ la-palette-du-gout.php
@@ -25,13 +25,99 @@
         </div>
         <section class="sectionMenu1">
             <h1>La Palette du gout</h1>
+
             <p class="subTtitle">ENTRÉES</p>
             <div class="horizontalGreen"></div>
+            <div class="cardMenuList flexbox">
+
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Fricassée d'escargot</h3>
+                        <p>Au piment d'Espelette</p>
+                    </div>
+                    <h3 class="prix">25€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Foie gras de canard mi-cuit</h3>
+                        <p>Et ses copeaux de truffe noire</p>
+                    </div>
+                    <h3 class="prix">35€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Oeuf au plat</h3>
+                        <p>Assaisonné à la truffe sur lit de caviar</p>
+                    </div>
+                    <h3 class="prix">20€</h3>
+                </div>
+
+            </div>
+            <p class="subTtitle">Plats</p>
+            <div class="horizontalGreen"></div>
+            <div class="cardMenuList flexbox">
+
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Filet de boeuf aux herbes</h3>
+                        <p>Accompagné de sa ribambelle de légumes</p>
+                    </div>
+                    <h3 class="prix aolute">40€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Parmentier de queue de boeuf</h3>
+                        <p>À la truffe noire sur sa purée de panais</p>
+                    </div>
+                    <h3 class="prix">35€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Filet de turbot</h3>
+                        <p>Aux agrumes</p>
+                    </div>
+                    <h3 class="prix">44€</h3>
+                </div>
+
+            </div>
+
+            <p class="subTtitle">Desserts</p>
+            <div class="horizontalGreen"></div>
+            <div class="cardMenuList flexbox">
+
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Paris-Brest</h3>
+                        <p>Revisité</p>
+                    </div>
+                    <h3 class="prix">18€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Macaron au chocolat d'exception </h3>
+                        <p>Et glace à la vanille de Madagascar</p>
+                    </div>
+                    <h3 class="prix">22€</h3>
+                </div>
+                <div class="card3 flexbox">
+                    <div class="card3Txt">
+                        <h3>Mousse au chocolat </h3>
+                        <p>Au piment d'Espelette et à la truffe noire</p>
+                    </div>
+                    <h3 class="prix">23€</h3>
+                </div>
+
+            </div>
+            <div class="commander">
+                <button class="cmdButton">Commander</button>
+            </div>
         </section>
 
 
 
     </main>
     
+    <?php include('components/footer.php'); ?>
+
 </body>
 </html>
