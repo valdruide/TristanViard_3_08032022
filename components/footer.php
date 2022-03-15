999063b582d063f226d89a1b4d530ca449a0ac44
Valdruide <tviardprimo190@gmail.com>
2022-03-15 13:23:39 +0100

    php to HTML

--- components/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-        <h2>Ohmyfood</h2>
-        <ul>
-            <li class="icoResto"><a href="#">Proposer un restaurant</a></li>
-            <li class="icoHand"><a href="#">Devenir partenaire</a></li>
-            <li><a href="#">Mentions légales</a></li>
-            <li><a href="mailto:test@test.com">Contact</a></li>
-        </ul>
-</footer>
